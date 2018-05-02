1820be414c3a57eae69c7623c75339dba38ee800
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-05-02 14:08:19 -0400

    fixed file path for app.js and created burgerbuilder component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Layout from '../components/Layout/Layout';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
